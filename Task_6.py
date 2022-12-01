cd4cfa02feb7767e7572a528f49e03615eeac50d
YanaVasileva <jana-ivanova@mail.ru>
2022-12-01 22:55:20 +0400

    Delete Task 6

--- Task_6.py
+++
@@ -1,9 +0,0 @@
-# Task 6.
-# Проверить ложность утверждения
-# (x ≡ z) V (x → (y Λ z))
-
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if not ((x == z) or (x <= y and z)):
-                print(x, y, z)
